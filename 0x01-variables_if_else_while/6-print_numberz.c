1657499d4e3aaf17fa78b5946f488bb104d3f7c2
sebbyapudo <sebbyapudo@gmail.com>
2023-03-20 12:58:25 -0700

    I need a girlfriend

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each time it's executed
+ * and print the last digit of the number stored in the variables n
+ * Return: always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+
+return (0);
+}
